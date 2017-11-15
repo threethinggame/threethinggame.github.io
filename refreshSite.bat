9996aeeac220776f137d44bb4001306bcfaf856b
David Parker <d.j.parker@hull.ac.uk>
2017-11-15 14:23:47 +0000

    added refresh script

---
+++ refreshSite.bat
@@ -0,0 +1,18 @@
+for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate set MyDate=%%x
+set today=%MyDate:~0,4%-%MyDate:~4,2%-%MyDate:~6,2%
+set commitMessage="site rebuild: %today%"
+echo %commitMessage%
+echo "Removing the old website"
+pushd public
+git rm -rf *
+popd
+
+echo "Building the website"
+hugo
+
+echo "Pushing the updated \`public\` folder to the \`master\` branch"
+pushd public
+git add *
+git commit -m %commitMessage%
+popd
+git push origin master
