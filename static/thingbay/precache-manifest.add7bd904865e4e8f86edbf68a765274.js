e85bf7882b1b9ba952af8ae7dc4d3d16335bc9b5
DavidParkerDr <david@goparker.com>
2020-07-02 20:19:03 +0100

    rebuilt thingbay

---
+++ static/thingbay/precache-manifest.add7bd904865e4e8f86edbf68a765274.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05be7dcf1cb940c8c355d51786c845d7",
+    "url": "/thingbay/index.html"
+  },
+  {
+    "revision": "3df16b2b47dbd8c365e9",
+    "url": "/thingbay/static/css/2.fb958ba7.chunk.css"
+  },
+  {
+    "revision": "1d283f12877027954f6d",
+    "url": "/thingbay/static/css/main.7ca84fb3.chunk.css"
+  },
+  {
+    "revision": "3df16b2b47dbd8c365e9",
+    "url": "/thingbay/static/js/2.eacf01e1.chunk.js"
+  },
+  {
+    "revision": "b3701772c9d18b1bed02fbe32b22c711",
+    "url": "/thingbay/static/js/2.eacf01e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d283f12877027954f6d",
+    "url": "/thingbay/static/js/main.06712099.chunk.js"
+  },
+  {
+    "revision": "e784f31a148c6c58857a",
+    "url": "/thingbay/static/js/runtime-main.fd3e1f8a.js"
+  },
+  {
+    "revision": "57ef87b8097ddab9107c41eb2f816164",
+    "url": "/thingbay/static/media/User.57ef87b8.svg"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/thingbay/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "2e99a728f5b426e2f0e31813f671de31",
+    "url": "/thingbay/static/media/waves.2e99a728.svg"
+  }
+]);
--- static/thingbay/service-worker.js
+++ static/thingbay/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef6d06ef8a2b70c40471f265d75697b8.js"
+  "/thingbay/precache-manifest.add7bd904865e4e8f86edbf68a765274.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thingbay/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
