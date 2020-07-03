5c7a954ae714ba81dd63748fe922bb2116461544
DavidParkerDr <david@goparker.com>
2020-07-03 23:00:48 +0100

    site rebuild: 2020-07-03

--- thingbay/service-worker.js
+++ thingbay/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thingbay/precache-manifest.add7bd904865e4e8f86edbf68a765274.js"
+  "/precache-manifest.e91e5c185a9c9f327543ea7aa90d79c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thingbay/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
